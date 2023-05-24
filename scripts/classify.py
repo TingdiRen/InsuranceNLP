869eb0796da70e3798ec9024d9dd9b90a3862fe2
IDEA <tingdiren@gmail.com>
2023-05-24 21:55:05 +0800

    first commit

--- scripts/classify.py
+++ scripts/classify.py
@@ -1,68 +1,66 @@
 import pandas as pd
 import os
-import sys
-import numpy as np
 import torch
+import numpy as np
 from tqdm import tqdm
-from torch.utils.data import DataLoader, Dataset
-from transformers import pipeline
-from collections import OrderedDict
-import torch.nn.utils.rnn as rnn_utils
-from transformers import ZeroShotClassificationPipeline
+from torch.utils.data import Dataset
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 import argparse
 import simple_icd_10 as icd
+from transformers import pipeline
 
 
-def load_args():
-    os.chdir('/home/develop/workspace/Insurance')
-    parser = argparse.ArgumentParser(description='Death Causes Classification')
-    parser.add_argument('--gpu_id', type=int, default=0, help='gpu id')
-    parser.add_argument('--batch_size', type=int, default=4, help='size for data_claim batch')
-    parser.add_argument('--chunk_size', type=int, default=4, help='size for data_claim chunking')
-    parser.add_argument('--data_dir', type=str, default='./data', help='directory of data_claim')
-    parser.add_argument('--save_dir', type=str, default='./results', help='directory of saving data_claim')
-    parser.add_argument('--pretrained_model', type=str, default='pretrained_cache/models--roberta_large',
-                        help='directory of pretrained model')
-    args = parser.parse_args()
-    args.device = f'cuda:{args.gpu_id}' if args.gpu_id >= 0 else 'cpu'
-    if args.chunk_size > 0 and args.gpu_id < 0:
-        raise RuntimeError('chunk_size is not supported for cpu')
-    return args
-
-
-class NLI_Dataset(Dataset):
-    def __init__(self, premise, hypothesis, prior_f=None):
+class Classify_Dataset(Dataset):
+    def __init__(self, reason1, reason2, premise, state, prior_f=None):
         if prior_f is not None:
-            priored_premise = prior_f(premise)
-        self.premise = [f'This example is {i}.' for i in priored_premise]
-        self.hypothesis = hypothesis
+            reason2 = prior_f(reason2)
+        reason2 = [i.split(":")[-1] for i in reason2]
+        concat_reason = [f"death by {i.lower()} and {j.lower()}" for i, j in zip(reason1, reason2)]
+        # condition = lambda x: x == 'entailment'
+        # pres = [f'This example is {i}.' for i in premise]
+        # hyps = list(reason1)
+
+        self.data = concat_reason
+        self.pres1 = list(premise)
+        self.i = 0
 
     def __len__(self):
-        return len(self.premise)
+        return len(self.data)
 
     def __iter__(self):
-        for p, h in zip(self.premise, self.hypothesis):
-            yield p, h
+        return self
+
+    # def __getitem__(self, idx):
+    #     h = self.data[idx]
+    #     p = self.pres1
+    #     return h, p
+    def __next__(self):
+        if self.i >= self.__len__():
+            self.i = 0
+            raise StopIteration
+
+        h = self.data[self.i]
+        p = self.pres1
+        self.i += 1
+        return h, p
 
 
 def prior_f(data):
     new_data = []
     data_icdo = pd.read_csv('data/ICD-O.csv')
-    dict_icdo = dict(zip(data_icdo['code'], data_icdo['value']))
+    dict_icdo = dict(zip(data_icdo['code'], data_icdo['value_cn']))
     '''分割code和des'''
     for i in data:
         code, desc = i.split(":")
-        if code[0] == 'M' and (not icd.is_valid_item(code)): # ICD-O的代码是错误的描述
+        if code[0] == 'M' and (not icd.is_valid_item(code)):  # ICD-O的代码是错误的描述
             desc = dict_icdo[code]
         new_data.append(desc)
     new_data = [i.split("，")[0] for i in new_data]
 
     medical_prior = {'蛛网膜': '脑和蛛网膜',
-                     '贲门': '胃和贲门',
                      '其他': 'other'}
     final_data = []
-    for i in tqdm(new_data, desc='[Perparing Data]'):
+    for i in new_data:
         tmp = [j for j in medical_prior.keys() if j in i]
         if len(tmp) > 0:
             new_i = i.replace(tmp[0], medical_prior[tmp[0]])
@@ -72,78 +70,67 @@ def prior_f(data):
     return final_data
 
 
-def infer_NLI(data):
-    '''
-    :param data:
-    :return:
-    '''
+def infer_classify(data, pred_cause_level):
+    namespaces = locals()
 
-    '''init dataset'''
-    dataloader = NLI_Dataset(premise=data['data_claim_death_cause_reason2'].to_list(),
-                             hypothesis=data['data_claim_death_cause_reason1'].to_list(),
-                             prior_f=prior_f)
-    '''init pretrained model'''
-    tokenizer = AutoTokenizer.from_pretrained(data['args'].pretrained_model)
-    nli_model = AutoModelForSequenceClassification.from_pretrained(data['args'].pretrained_model).to(
-        data['args'].device)
-
-    res = []
-    data_claim_reason_compare = data['data_claim'].copy()
-    for p, h in tqdm(dataloader, desc='[TASK] NLI'):
-        input = tokenizer.encode(h, p, return_tensors='pt').to(data['args'].device)
-        pred = nli_model(input)
-        contradiction, neutral, entailment = torch.nn.functional.softmax(pred.logits[0], dim=0)
-        res.append((contradiction.item(), neutral.item(), entailment.item()))
-    data_claim_reason_compare['contradiction'], data_claim_reason_compare['neutral'], data_claim_reason_compare[
-        'entailment'] = zip(*res)
-    data_claim_reason_compare['state'] = data_claim_reason_compare.iloc[:, -3:].idxmax(axis=1)
-    data_claim_reason_compare['prob'] = data_claim_reason_compare.iloc[:, -4:-1].max(axis=1)
-    data_claim_reason_compare.to_csv(os.path.join(data['args'].save_dir, f'NLI_chunk_{data["args"].gpu_id}.csv'),
-                                     encoding='utf_8_sig', index=None)
-    return data_claim_reason_compare
-
-
-def infer_Classify(data, reason_level=1, cause_level=2):
-    '''
-    :param data:
-    :param reason_level:
-    :param cause_level:
-    :return:
-    '''
-
-    '''init dataset'''
-    dataloader = NLI_Dataset(premise=[i.split(":")[1] for i in data[f'list_ruled_death_cause_level{cause_level}']],
-                             hypothesis=data[f'data_claim_death_cause_reason{reason_level}'].to_list())
+    # for level in range(1, 4):
+    #     '''X级死因: list'''
+    #     namespaces[f'premise_level{level}'] = [f'This example is {i}.' for i in data[f"list_ruled_death_cause_level{level}"]]
 
     '''init pretrained model'''
-    tokenizer = AutoTokenizer.from_pretrained(data['args'].pretrained_model)
-    nli_model = AutoModelForSequenceClassification.from_pretrained(data['args'].pretrained_model).to(
-        data['args'].device)
+    classifier1 = pipeline("zero-shot-classification", model='./pretrained_cache/models--roberta_large',
+                           device=data['args'].gpu_id, batch_size=data['args'].batch_size, multi_label=True)
+    # nli_model = pipeline("zero-shot-classification", model=data['args'].pretrained_model, batch_size=8)
+    classifier2 = pipeline("zero-shot-classification", model="MoritzLaurer/mDeBERTa-v3-base-mnli-xnli",
+                           device=data['args'].gpu_id, batch_size=data['args'].batch_size, multi_label=True)
 
-    '''infer with chunk'''
+    '''init dataset'''
+    dataloader = Classify_Dataset(reason1=data['data_claim_death_cause_reason1'].to_list(),
+                                  reason2=data['data_claim_death_cause_reason2'].to_list(),
+                                  premise=data[f"list_ruled_death_cause_level{pred_cause_level}"],
+                                  state=data['data_claim_state'].to_list(),
+                                  prior_f=prior_f)
+
+    '''infer'''
     res = []
-    data_claim_classify = data['data_claim'].copy()
-    for p, h in tqdm(dataloader, desc='[TASK] Classify'):
-        input = tokenizer.encode(h, p, return_tensors='pt').to(data['args'].device)
-        pred = nli_model(input)
-        contradiction, neutral, entailment = torch.nn.functional.softmax(pred.logits[0])
-        res.append(dict(zip(pred['labels'], pred['scores'])))
+    data_claim_reason_classify = data['data_claim'].copy()
+    # for level in range(1, 4):
+    # premise_lists = namespaces[f'premise_level{level}']
+    # namespaces[f'prob_causelevel{level}'] = []
+    # namespaces[f'result_causelevel{level}'] = []
+    prob1_cause, result1_cause = [], []
+    prob2_cause, result2_cause = [], []
+    for h, ps in tqdm(dataloader, desc=f'[TASK] Classify [LEVEL] {pred_cause_level}'):
+        pred1 = classifier1(h, ps)
+        pred2 = classifier2(h, ps)
+        # contradiction, neutral, entailment = pred1.logits[0]
+        # prob.append(entailment.item())
+        # prob = torch.nn.functional.softmax(torch.tensor(prob), dim=0)
+        prob1_cause.append(pred1['scores'])
+        result1_cause.append(pred1['labels'][np.argmax(pred1['scores'])])
+        prob2_cause.append(pred2['scores'])
+        result2_cause.append(pred2['labels'][0])
+    data_claim_reason_classify[f'prob1_causelevel{pred_cause_level}'] = prob1_cause
+    data_claim_reason_classify[f'result1_causelevel{pred_cause_level}'] = result1_cause
+    data_claim_reason_classify[f'prob2_causelevel{pred_cause_level}'] = prob2_cause
+    data_claim_reason_classify[f'result2_causelevel{pred_cause_level}'] = result2_cause
+    data_claim_reason_classify.to_csv(
+        os.path.join(data['args'].save_dir, f'Classify_chunk_causelevel{pred_cause_level}_{data["args"].gpu_id}.csv'),
+        encoding='utf_8_sig', index=None)
+    return data_claim_reason_classify
 
-    data_claim_classify['pred'] = res
-    data_claim_classify.to_csv(os.path.join(data['args'].save_dir, f'Classify_chunk_{data["args"].gpu_id}.csv'),
-                               encoding='utf_8_sig', index=None)
-    return data_claim_classify
 
-
-def concat_df(args, chunk_head='NLI'):
-    raw_data = pd.read_excel(os.path.join(args.data_dir, '1.理赔数据.xlsx'))
+def concat_df(args, cause_level, chunk_head='Classify'):
+    raw_data = pd.read_csv(os.path.join(args.data_dir, 'NLI_final_res.csv'))
     raw_data = raw_data.drop(list(raw_data.columns)[1:], axis=1)
     concat_list = []
     for chunk in range(args.chunk_size):
-        concat_list.append(pd.read_csv(os.path.join(args.save_dir, f'{chunk_head}_chunk_{chunk}.csv')))
+        concat_list.append(
+            pd.read_csv(os.path.join(args.save_dir, f'{chunk_head}_chunk_causelevel{cause_level}_{chunk}.csv')))
     concated_df = pd.concat(concat_list, axis=0)
     sorted_concated_df = pd.merge(raw_data, concated_df, on='ID')
-    sorted_concated_df.to_csv(os.path.join(args.save_dir, f"{chunk_head}_final_res.csv"), encoding='utf_8_sig',
+    sorted_concated_df.to_csv(os.path.join(args.save_dir, f"{chunk_head}_causelevel{cause_level}_final_res.csv"),
+                              encoding='utf_8_sig',
                               index=None)
     return sorted_concated_df
 
@@ -151,28 +138,78 @@ def concat_df(args, chunk_head='NLI'):
 def read_data(args):
     namespaces = locals()
     '''理赔数据: df'''
-    namespaces['data_claim'] = pd.read_excel(os.path.join(args.data_dir, '1.理赔数据.xlsx'))
-    # namespaces['data_claim'] = pd.read_csv(os.path.join(args.data_dir, 'claim_ENreason.csv'))
+    # namespaces['data_claim'] = pd.read_csv(os.path.join(args.data_dir, 'NLI_final_res.csv'))
+    namespaces['data_claim'] = pd.read_csv(os.path.join(args.data_dir, 'unsuper1.csv'))
+    # namespaces['data_claim'] = namespaces['data_claim'][namespaces['data_claim']['state'] == 'entailment']
+    # namespaces['data_claim'] = namespaces['data_claim'][namespaces['data_claim'].isnull().any(axis=1)]
+    # namespaces['data_claim'] = namespaces['data_claim'][namespaces['data_claim']['reason_level2'] == '肿瘤']
     if args.chunk_size > 0:
         namespaces['data_claim'] = namespaces['data_claim'][args.gpu_id::args.chunk_size]
 
+    namespaces['data_claim_state'] = namespaces['data_claim']['state']
     for level in range(1, 3):
         '''Reason1.2, Series'''
-        namespaces[f"data_claim_death_cause_reason{level}"] = namespaces['data_claim'][f'Reason{level}']
+        namespaces[f"data_claim_death_cause_reason{level}"] = namespaces['data_claim'][f'Reason{level}_EN']
 
     '''死因: df'''
     namespaces['data_ruled_death_cause'] = pd.read_excel(os.path.join(args.data_dir, '字段解释.xlsx'), sheet_name=2)
     for level in range(1, 4):
         '''X级死因: list'''
         namespaces[f"list_ruled_death_cause_level{level}"] = namespaces[
-                                                                 'data_ruled_death_cause'].iloc[:, level - 1].to_list()
+                                                                 'data_ruled_death_cause'].iloc[:,
+                                                             level - 1].dropna().to_list()
 
+    namespaces[f"list_ruled_death_cause_level2"] = pd.Series(
+        ["death declared by court's judgment",
+         "death by homicide",
+         "death by traffic accidents",
+         "death by unknown accidents without causes",
+         "diseases of tumour",
+         "diseases of circulatory system and blood system",
+         "diseases of respiratory system",
+         "diseases of digestive system", "diseases of urinary and reproductive systems",
+         "diseases of nervous or motor system",
+         "diseases of endocrine or immune or metabolic system",
+         "simply diseases",
+         "unknown death without causes",
+         "natural death",
+         "death or injury by falling",
+         "death or injury by drowning",
+         "death or injury by suffocation",
+         "death or injury by fire",
+         "death or injury by scald",
+         "death or injury by electric currents, radiation, and extreme environments",
+         "death or injury by natural disaster",
+         "death or injury by poisonous plant and animal",
+         "death or injury by toxic chemicals",
+         "death or injury by excessive physical exertion, starvation, thirst",
+         "died during surgery"])
+    namespaces[f"list_ruled_death_cause_level1"] = pd.Series(
+        ["death by disease", "death by accident", "deliberate suicide", "death declared by court's judgment",
+         "natural death"])
     return namespaces
+
+
+def load_args():
+    os.chdir('/home/develop/workspace/Insurance')
+    parser = argparse.ArgumentParser(description='Death Causes Classification')
+    parser.add_argument('--gpu_id', type=int, default=0, help='gpu id')
+    parser.add_argument('--batch_size', type=int, default=16, help='size for data_claim batch')
+    parser.add_argument('--chunk_size', type=int, default=4, help='size for data_claim chunking')
+    parser.add_argument('--data_dir', type=str, default='./data', help='directory of data_claim')
+    parser.add_argument('--save_dir', type=str, default='./results', help='directory of saving data_claim')
+    parser.add_argument('--pretrained_model', type=str, default='pretrained_cache/models--roberta_large',
+                        help='directory of pretrained model')
+    parser.add_argument('--pred_cause_level', type=int, default=2, help='directory of pretrained model')
+    args = parser.parse_args()
+    args.device = f'cuda:{args.gpu_id}' if args.gpu_id >= 0 else 'cpu'
+    if args.chunk_size > 0 and args.gpu_id < 0:
+        raise RuntimeError('chunk_size is not supported for cpu')
+    return args
 
 
 if __name__ == '__main__':
     args = load_args()
-    # data_claim = read_data(args)
-    concat_df(args, chunk_head='NLI')
-    # data_claim_reason_compare = infer_NLI(data_claim)
-    # data_claim_classify = infer_classify(data_claim)
+    data_claim = read_data(args)
+    data_claim_reason_classify = infer_classify(data_claim, args.pred_cause_level)
+    # concat_df(args, cause_level=args.pred_cause_level, chunk_head='Classify')
---
+++ scripts/classify_gpt.py
@@ -0,0 +1,189 @@
+import pandas as pd
+import os
+from tqdm import tqdm
+import time
+import argparse
+import simple_icd_10 as icd
+import requests
+from requests.exceptions import Timeout, HTTPError
+
+def query(payload, API_URL, headers):
+    while True:
+        try:
+            response = requests.post(API_URL, headers=headers, json=payload, timeout=2)
+            if response.status_code == 200:
+                break
+            else:
+                continue
+        except Timeout or HTTPError:
+            continue
+    return response.json()
+
+
+class Classify_Dataset:
+    def __init__(self, reason1, reason2, level, pred_cause_level2=None):
+        self.init_template()
+
+        self.level = level
+        self.reason1 = reason1
+        self.reason2 = self.prior_f(reason2)
+        if level == 3:
+            self.pred_cause_level2 = pred_cause_level2
+
+        self.i = 0
+
+    def __next__(self):
+        if self.i >= self.__len__():
+            self.i = 0
+            raise StopIteration
+
+        data = f'Please answer to the following question. If someone dead with symptom "{self.reason2[self.i].lower()}", which is caused by event "{self.reason1[self.i].lower()}", what\'s the proximate cause of the death in insurance? ' \
+               f'Chose an answer from {self.get_cause(self.i)}.'
+        self.i += 1
+        return data
+
+    def get_cause(self, index):
+        if self.level == 2:
+            return self.cause_level2
+        elif self.level == 3:
+            pred_cause_level2 = self.pred_cause_level2[index]
+            return self.cause_level3_dict[pred_cause_level2]
+
+    def init_template(self):
+        self.cause_level2 = '["Declared dead by the court", ' \
+                            '"Intentional suicide", "Homicide", ' \
+                            '"Accidents caused by non-vehicles", "Accidents caused by vehicles", "Unexplained accident", ' \
+                            '"Tumor", "Diseases of circulatory and blood system", "Diseases of Respiratory system", ' \
+                            '"Diseases of the digestive system", "Diseases of the urinary and reproductive system", ' \
+                            '"Diseases of the nervous and motor systems", "Endocrine/immune/metabolic diseases", ' \
+                            '"Diseases that cannot be classified as tumors or a specific system", ' \
+                            '"Unable to determine the disease that caused the death"]'
+
+        self.cause_level3_dict = {"Declared dead by the court": '["Declared dead by the court"]',
+                                  "Intentional suicide": '["Intentional suicide"]',
+                                  "Homicide": '["Homicide"]',
+                                  "Non-Traffic Accident": '["Falling down and falling","Inanimate mechanical force","With life mechanical force","Drowning","Suffocation","Fire","Scalding","Electric current, radiation and extreme environment","Natural disasters","Toxic plants and animals","Toxic chemicals","Death from physical overexertion","Death from accidents during medical treatment"]',
+                                  "Traffic Accident": '["Pedestrians","Non-motorized vehicles","Two-wheeled motor vehicles","Three-wheeled motor vehicles","Car","Passenger car","Lorry","Other road traffic","Rail Transportation","Water Transportation","Air traffic", "unknown traffic]',
+                                  "Unexplained accident": '["Unexplained accident"]',
+                                  "Tumor": '["Malignant Tumors","Benign brain tumors","Hemangioma or aneurysm","Other benign tumors"]',
+                                  "Diseases of circulatory and blood system": '["Acute myocardial infarction","Cerebral Stroke","Coronary heart disease","Aortic disease","Heart valve disease","Heart disease of pulmonary origin","Heart Inflammation","Aplastic anemia","Other circulatory and blood disorders"]',
+                                  "Diseases of the digestive system": '["Acute Liver Failure","Chronic liver failure","Crohn\'s disease and ulcerative colitis","Pancreatitis","Gallbladder inflammation","Other digestive system diseases"]',
+                                  "Diseases of Respiratory system": '["Acute respiratory failure, influenza, acute pneumonia","Chronic respiratory failure, chronic obstructive pulmonary disease, asthma, pulmonary fibrosis progression of respiratory failure"]',
+                                  "Diseases of the urinary and reproductive system": '["Acute renal failure, acute renal failure due to inadequate renal blood supply or abnormal renal excretory function","Chronic renal failure, uremia","Obstetric diseases	","Sexually transmitted diseases, excluding AIDS","Other urological and reproductive system diseases"]',
+                                  "Diseases of the nervous and motor systems": '["Alzheimer\'s disease and other dementias","Parkinson\'s disease","Epilepsy","Motor neuron disease","Multiple sclerosis","Myasthenia Gravis","Myotonic Dystrophy","Encephalitis and Meningitis","Other neurological and motor system diseases"]',
+                                  "Endocrine/immune/metabolic diseases": '["Diabetes","Lupus Erythematosus","Rheumatoid Arthritis","Scleroderma","AIDS","Other endocrine/immune/metabolic diseases"]',
+                                  "Diseases that cannot be classified as tumors or a specific system": '["Diseases that cannot be classified as tumors or a specific system"]',
+                                  "Unable to determine the disease that caused the death": '["Unable to determine the disease that caused the death"]'}
+
+    def prior_f(self, data):
+        new_data = []
+        data_icdo = pd.read_csv('data/ICD-O.csv')
+        dict_icdo = dict(zip(data_icdo['code'], data_icdo['value_cn']))
+        '''split code and des'''
+        for i in data:
+            code, desc = i.split(":")
+            if code[0] == 'M' and (not icd.is_valid_item(code)):  # ICD-O code's description is wrong
+                desc = dict_icdo[code]
+            new_data.append(desc)
+        new_data = [i.split("，")[0].split(":")[-1] for i in new_data]
+        return new_data
+
+    def __len__(self):
+        return len(self.reason1)
+
+    def __iter__(self):
+        return self
+
+
+def infer_classify(data, pred_cause_level):
+    namespaces = locals()
+
+    '''init infer API'''
+    API_URL = "https://api-inference.huggingface.co/models/google/flan-ul2"
+    API_TOKEN = data['args'].API_TOKEN
+    headers = {"Authorization": f"Bearer {API_TOKEN}"}
+
+    '''init dataset'''
+    if pred_cause_level == 2:
+        dataloader = Classify_Dataset(reason1=data['data_claim_death_cause_reason1'].to_list(),
+                                      reason2=data['data_claim_death_cause_reason2'].to_list(),
+                                      level=pred_cause_level)
+    elif pred_cause_level == 3:
+        dataloader = Classify_Dataset(reason1=data['data_claim_death_cause_reason1'].to_list(),
+                                      reason2=data['data_claim_death_cause_reason2'].to_list(),
+                                      level=pred_cause_level,
+                                      pred_cause_level2=data['data_claim_death_cause_level2']['resultGPT_causelevel2'].to_list())
+
+    '''infer'''
+    result_cause = []
+    data_claim_reason_classify = data['data_claim'].copy()
+    for q in tqdm(dataloader, desc=f'[Chunk] {data["args"].chunk_id} [LEVEL] {pred_cause_level}'):
+        output = query({"inputs": q}, API_URL, headers)  # request for inference API
+        while 'error' in output:
+            output = query({"inputs": q}, API_URL, headers)
+            time.sleep(10)
+        result_cause.append(output[0]['generated_text'])
+    data_claim_reason_classify[f'resultGPT_causelevel{pred_cause_level}'] = result_cause
+    data_claim_reason_classify.to_csv(
+        os.path.join(data['args'].save_dir, f'Classify_chunk_causelevel{pred_cause_level}_{data["args"].chunk_id}.csv'),
+        encoding='utf_8_sig', index=None)
+    return data_claim_reason_classify
+
+
+def concat_df(args, cause_level, chunk_head='Classify'):
+    raw_data = pd.read_csv(os.path.join(args.data_dir, 'processed_claim.csv'))
+    raw_data = raw_data.drop(list(raw_data.columns)[1:], axis=1)
+    concat_list = []
+    for chunk in range(args.chunk_size):
+        concat_list.append(
+            pd.read_csv(os.path.join(args.save_dir, f'{chunk_head}_chunk_causelevel{cause_level}_{chunk}.csv')))
+    concated_df = pd.concat(concat_list, axis=0)
+    sorted_concated_df = pd.merge(raw_data, concated_df, on='ID')
+    sorted_concated_df.to_csv(os.path.join(args.save_dir, f"{chunk_head}_causelevel{cause_level}_final_res.csv"),
+                              encoding='utf_8_sig', index=None)
+    return sorted_concated_df
+
+
+def read_data(args):
+    namespaces = locals()
+    namespaces['data_claim'] = pd.read_csv(os.path.join(args.data_dir, 'processed_claim.csv'))[
+                               args.chunk_id::args.chunk_size]
+
+    for level in range(1, 3):
+        '''Reason1.2, Series'''
+        namespaces[f"data_claim_death_cause_reason{level}"] = namespaces['data_claim'][f'Reason{level}_EN']
+
+    if args.pred_cause_level == 3:
+        namespaces[f"data_claim_death_cause_level2"] = pd.read_csv(
+            os.path.join(args.data_dir, 'Classify_causelevel2_final_res.csv'))[args.chunk_id::args.chunk_size]
+    return namespaces
+
+
+def load_args():
+    #os.chdir('../')
+    os.chdir('/home/develop/workspace/Insurance')
+    parser = argparse.ArgumentParser(description='Death Causes Classification')
+    parser.add_argument('--chunk_id', type=int, default=0, help='chunk id')
+    parser.add_argument('--batch_size', type=int, default=16, help='size for data_claim batch')
+    parser.add_argument('--chunk_size', type=int, default=8, help='size for data_claim chunking')
+    parser.add_argument('--data_dir', type=str, default='./data', help='directory of data_claim')
+    parser.add_argument('--save_dir', type=str, default='./results', help='directory of saving data_claim')
+    parser.add_argument('--pretrained_model', type=str, default='pretrained_cache/models--roberta_large',
+                        help='directory of pretrained model')
+    parser.add_argument('--API_TOKEN', type=str, default='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',
+                        help='api token for hugging face')
+    parser.add_argument('--pred_cause_level', type=int, default=3, help='level for pred cause')
+    parser.add_argument('--concat', type=bool, default=False, help='concat chunk results')
+    args = parser.parse_args()
+    if args.chunk_size > 0 and args.chunk_id < 0:
+        raise RuntimeError('chunk_size is not supported for cpu')
+    return args
+
+
+if __name__ == '__main__':
+    args = load_args()
+    if args.concat:
+        concat_df(args, cause_level=args.pred_cause_level, chunk_head='Classify')
+    else:
+        data_claim = read_data(args)
+        data_claim_reason_classify = infer_classify(data_claim, args.pred_cause_level)
